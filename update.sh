49972ccc4b2003e24838c55679240c028e617f0e
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-08-14 20:18:00 +0200

    Update update.sh to build all data

--- update.sh
+++ update.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-echo "Updating generated JSON..."
+echo "Updating"
+
+echo "Updating English..."
+echo "Updating JSON..."
 dhall-to-json <<< '(./dhall/content/english.dhall).me' > ./data/english.json
+echo "Updating TeX..."
+dhall text <<< '(./dhall/content/english.dhall).resume' > ./data/english.tex
+echo "Finished English!"
+
+echo "Updating Norwegian..."
+echo "Updating JSON..."
 dhall-to-json <<< '(./dhall/content/norwegian.dhall).me' > ./data/norwegian.json
+echo "Updating TeX..."
+dhall text <<< '(./dhall/content/norwegian.dhall).resume' > ./data/norwegian.tex
+echo "Finished Norwegian!"
+
+echo "Rendering to PDF"
+lualatex -pdf ./data/english.tex
+lualatex -pdf ./data/norwegian.tex
+echo "Finished rendering"
+
 echo "Finished updating..."
