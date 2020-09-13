025684a571649f254d33d454046c0252afd8fc4c
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-09-13 18:11:13 +0200

    Create copies of CV that are properly named

--- update.sh
+++ update.sh
@@ -22,4 +22,8 @@ lualatex -pdf -interaction=nonstopmode english.tex
 lualatex -pdf -interaction=nonstopmode norwegian.tex
 echo "Finished rendering"
 
+echo "Creating properly named CVs"
+cp norwegian.pdf SondreNilsenCV.pdf
+cp english.pdf SondreNilsenCV-en.pdf
+
 echo "Finished updating..."
