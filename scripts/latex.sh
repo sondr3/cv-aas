7ed986720d0f84a93564a3e7cd14279c2f7d550a
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-10-10 19:35:40 +0200

    Create release with binary and PDFs
    Former-commit-id: ab5dbb4d1da8956f4cec9a1d56c4f1fae085eefd

--- scripts/latex.sh
+++ scripts/latex.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Rendering to PDF"
-cd ./data
+cd ./data || exit
 lualatex -pdf -interaction=nonstopmode english.tex
 lualatex -pdf -interaction=nonstopmode norwegian.tex
 echo "Finished rendering"
 
 echo "Creating properly named CVs"
 cp norwegian.pdf SondreNilsenCV.pdf
-cp english.pdf SondreNilsenCV-en.pdf
+cp english.pdf SondreNilsenCV-English.pdf
 
 echo "Finished updating..."
