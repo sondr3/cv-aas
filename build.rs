e0ac1137c7e69d3852d135fc1d240007c3aae183
Sondre Nilsen <sondre@eons.io>
2021-10-10 15:02:03 +0200

    Touch the PDFs so building in debug works anywhere

--- build.rs
+++ build.rs
@@ -1,14 +1,28 @@
-use std::process::{Command, Stdio};
+use std::{
+    env,
+    process::{Command, Stdio},
+};
 
 fn main() {
     println!("cargo:rerun-if-changed=dhall");
     println!("cargo:rerun-if-changed=data/template.cls");
     println!("cargo:rerun-if-changed=data/me.jpg");
 
-    Command::new("./update.sh")
-        .stdin(Stdio::null())
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .spawn()
-        .expect("Could not build PDF and JSON");
+    if Ok("release".to_string()) == env::var("PROFILE") {
+        Command::new("./update.sh")
+            .stdin(Stdio::null())
+            .stderr(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .spawn()
+            .expect("Could not build PDF and JSON");
+    } else {
+        Command::new("touch")
+            .arg("data/english.pdf")
+            .spawn()
+            .unwrap();
+        Command::new("touch")
+            .arg("data/norwegian.pdf")
+            .spawn()
+            .unwrap();
+    }
 }
